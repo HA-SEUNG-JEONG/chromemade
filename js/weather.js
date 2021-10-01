7add3ebeceed95b9b9ef86bf9383e3f709260a0d
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-10-01 11:31:31 +0900

    first experiment

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "29e1d170a9465e17ac63ed52ad2ad863";
+function GeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function GeoError() {
+  alert("Can't Find you.");
+}
+
+navigator.geolocation.getCurrentPosition(GeoOk, GeoError);
